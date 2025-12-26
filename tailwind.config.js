b37d42882f32e5fa35f68619a9ae3a35d2990e88
İbrahim Kiriş <kirisibrahim2020@gmail.com>
2025-12-27 01:35:04 +0300

    babel ve yol çözümleme sorunları giderildi uygulama ayağa kaldırıldı

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Uygulamanın hangi klasörlerdeki stilleri tarayacağını seçiyoruz
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
